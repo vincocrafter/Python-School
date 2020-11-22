91f04ec228c355e10e4918eb02a6e3e4fabd5107
VincoDev <vincodev@gmail.com>
2020-11-22 17:59:38 +0100

    Update

--- Examples.py
+++ Examples.py
@@ -1,4 +1,4 @@
 #Hier werden ersmal Code-Konventions erklärt
 # Variablennamen schreibt man IMMER klein --> variablenname = variable <-- Variablenwert kommt RECHTS von dem Gleichzeichen
 # def methode(): Methodennamen schreibt man ebenfalls IMMER klein
-#Nur Python Skriptnamen schreibt man IMMER groß
+#Nur Python Skriptnamen schreibt man IMMER groß --> Hier Examples.py
