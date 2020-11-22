06069b8fa436e0829e68d05d56884f2eb31cea06
VincoDev <vincodev@gmail.com>
2020-11-22 17:51:49 +0100

    Update

--- führerscheintest.py
+++ führerscheintest.py
@@ -1,8 +1,9 @@
-def test(alter) :
+def prüfung(alter) :
     if alter >= 16:
         print("Ja, du darfst den Führerschein machen.")
     else:
         print("Sorry, du bleibst in der Warteschleife!")
 
-def test():
-    print("Testing...")
+def start():
+    print("Testing...123")
+    prüfung(18)
