d260a5a93a627bada392ef990e17e40f7782039e
VincoDev <vincodev@gmail.com>
2021-01-08 14:37:36 +0100

    Arbeitsauftrag

--- gui/Abschnitt5.py
+++ gui/Abschnitt5.py
@@ -6,10 +6,23 @@ from tkinter import *
 import tkinter
 
 
+def test1():
+    print("Test1")
+
+
 root = Tk()
 
 root.title("Test")
 
+button1 = Button(text="Test", bg="white", fg="black", command=test1)
+button1.grid()
 
+var1 = IntVar()
+
+checkbutton1 = Checkbutton(root, text="Test", onvalue=1, offvalue=0, variable=var1)
+checkbutton1.grid()
+
+radiobutton1 = Radiobutton(root, text="Test", value=0)
+radiobutton1.grid()
 
 root.mainloop()
